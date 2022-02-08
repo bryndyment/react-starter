bc352229d7dc96cc4820891e3deb08888ad70a9f
Bryn Dyment <bryndyment@gmail.com>
2022-02-08 11:26:12 -0800

    more cleanup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import './index.css'
-import App from './App'
+import { BrowserRouter } from 'react-router-dom'
+import { RouteList } from './RouteList'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <RouteList />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 )
