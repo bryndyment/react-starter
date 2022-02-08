3df3c151abe0dd60263ddb291904a0d052ca657e
Bryn Dyment <bryndyment@gmail.com>
2022-02-08 08:08:21 -0800

    cleanup

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import logo from './logo.svg'
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img alt="logo" className="App-logo" src={logo} />
+
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+
+        <a className="App-link" href="https://reactjs.org" rel="noopener noreferrer" target="_blank">
+          Learn React
+        </a>
+      </header>
+    </div>
+  )
+}
+
+export default App
